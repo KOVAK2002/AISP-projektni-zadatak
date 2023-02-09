fdab6bc96e17d8e9f8f51b3666d3f0013d755984
KOVAK2002 <123957703+KOVAK2002@users.noreply.github.com>
2023-02-09 14:06:54 +0100

    Update main.cpp
    Korištenje klase čiji su podaci definirani kao key, value. Korištenje mape kao strukture podatka. 2/9/2023-14.06 h

--- main.cpp
+++ main.cpp
@@ -2,44 +2,95 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <sstream>
- 
+#include <sstream> 
+#include <unordered_map>
+#include <map>
+#include <iomanip>
+
+using std::map;
+using std::string;
+using std::vector;
+using namespace std::literals;
 using namespace std;
- 
+class Fraud
+{
+public:
+    void get_top_values(int n);
+    void get_min_values(int n);
+
+    int stupac1;
+    vector<string> stupac2;
+    map<int, vector<string>> redci;
+};
+
+void printVector(vector<int> &data)
+{
+    for (const auto &it : data)
+    {
+        std::cout << it << " ";
+    }
+    std::cout << std::endl;
+}
+
 int main()
 {
-string fname="fraudTest.csv";
 
- 
-vector<vector<string>> content;
-vector<string> row;
-string line, word;
- 
-fstream file (fname, ios::in);
-if(file.is_open())
-{
-while(getline(file, line))
-{
-row.clear();
- 
-stringstream str(line);
- 
-while(getline(str, word, ','))
-row.push_back(word);
-content.push_back(row);
+
+    string fname="fraudTest.csv";
+
+
+
+
+    vector<int> v1 = {5, 6, 7};
+    map<int, vector<int>> data;
+    data.insert(std::make_pair(1, v1));
+
+
+    
+    vector<vector<string>> content;
+    vector<string> row;
+    string line, word;
+
+       
+                fstream file (fname, ios::in);
+        if(file.is_open())
+        {
+        while(getline(file, line))
+        {
+        row.clear();
+        
+        stringstream str(line);
+        
+        while(getline(str, word, ','))
+        row.push_back(word);
+        content.push_back(row);
+        }
+        }
+        else
+        cout<<"Could not open the file\n";
+        
+        for(int i=1;i<17;i++)
+        {
+        for(int j=0;j<14;j++)
+        {
+        cout<<content[i][j]<<" ";
+        }
+        cout<<"\n";
+        }
+
+            
+
+
+
+
+
+    for (auto it = data.begin(); it != data.cend(); it++)
+    {
+        std::cout << it->first << ": ";
+        printVector(it->second);
+    }
 }
-}
-else
-cout<<"Could not open the file\n";
- 
-for(int i=0;i<content.size();i++)
-{
-for(int j=0;j<content[i].size();j++)
-{
-cout<<content[i][j]<<" ";
-}
-cout<<"\n";
-}
+
  
 return 0;
 }
